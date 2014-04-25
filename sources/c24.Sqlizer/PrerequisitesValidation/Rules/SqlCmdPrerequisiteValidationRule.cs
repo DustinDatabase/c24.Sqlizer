1c301fd0689f9a6871be0ac9a12daa8c5e94d054
Aleksander Taran <aleksander.taran@check24.de>
2014-04-25 16:00:48 +0200

    Improve sqlcmd prerequisite validation

--- sources/c24.Sqlizer/PrerequisitesValidation/Rules/SqlCmdPrerequisiteValidationRule.cs
+++ sources/c24.Sqlizer/PrerequisitesValidation/Rules/SqlCmdPrerequisiteValidationRule.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 
+using c24.Runman;
 using c24.Sqlizer.Exceptions;
 
 using Microsoft.Win32;
@@ -11,24 +12,17 @@ namespace c24.Sqlizer.PrerequisitesValidation.Rules
     {
         public void Validate()
         {
-            var clientSetupKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64)
-                .OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server\100\Tools\ClientSetup");
-            if (clientSetupKey == null)
-            {
-                throw new SqlCmdNotFoundException("MS SQL client tools are not registered");
-            }
-            var sqlClientFolderPath = (string)clientSetupKey.GetValue("Path");
-            
-            if (sqlClientFolderPath == null)
-            {
-                throw new SqlCmdNotFoundException("MS SQL client tools are not registered");
-            }
+            var startInfo = CommandLineProgram.Prepare()
+               .WithFileName(@"sqlcmdbb.exe")
+               .WithArguments(new []{ "-?" });
 
-            var sqlCmdPath = Path.Combine(sqlClientFolderPath, "sqlcmd.exe");
-
-            if (!File.Exists(sqlCmdPath))
+            try
             {
-                throw new SqlCmdNotFoundException("SQLCMD.EXE not found in the system");
+                CommandLineProgram.Run(startInfo);
+            }
+            catch (System.ComponentModel.Win32Exception)
+            {
+                throw new SqlCmdNotFoundException("SQLCMD is not found in your system");
             }
         }
     }
