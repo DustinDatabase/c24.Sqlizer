1c4c509b0b55012c7b4bcd862a9356d03d2cc66c
Ilker Cetinkaya <ilker@ilker.de>
2014-09-22 21:39:59 +0200

    Introduce FAKE build

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NUGET=./.nuget/nuget.exe
+FAKE=./packages/FAKE.Core/tools/FAKE.exe
+
+[ ! -e $FAKE ] && mono $NUGET install FAKE.Core -ExcludeVersion -NonInteractive -OutputDirectory ./packages
+mono $FAKE ./build.fsx $@
+
